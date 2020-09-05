4ecb915e788f79e1bd379f3f89d227a82bff9b3e
kaichihasegawa1220 <sayakaichi@icloud.com>
2020-09-05 17:17:09 +0900

    追加１

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("../checked")
 require("../memo")
-
+require('jquery')
+require('../articles')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
